9b295e8630002d9416cb95bd5c32bf0ffbc6abe9
umut21 <umutyikilmaz4@gmail.com>
2026-07-15 23:55:53 +0300

    feat: add restaurant menu system

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdirmaKomutlari
+            //Console.WriteLine("Merhaba Dünya!");
+            //Console.Write("Selam");
+
+            //Console.WriteLine("----  Yiyecek/İçecek Çeşitleri ----");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Yiyecekler");
+            //Console.WriteLine("2-İçecekler");
+            //Console.WriteLine("3-Tatlılar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine();
+            //Console.WriteLine("----  Yiyecek/İçecek Çeşitleri ----");
+            #endregion
+
+            #region Değişkenler
+            //Değişkenler
+
+            //String
+            //Değişken_türü Değişken_adı;
+
+            //string name ;
+            //name = "Umut";
+            //Console.Write(name);
+
+            string customerName;
+            string customerSurname;
+            string customerPhone;
+            string customerEmail, district, city;
+
+            Console.Write("Adınızı Girin: ");
+            customerName = Console.ReadLine();
+            Console.Write("Soyadınızı Girin: ");
+            customerSurname = Console.ReadLine();
+            Console.Write("Telefon Numaranızı Girin: ");
+            customerPhone = Console.ReadLine();
+            Console.Write("Emalinizi Girin: ");
+            customerEmail = Console.ReadLine();
+            Console.Write("İlçenizi Girin: ");
+            district = Console.ReadLine();
+            Console.Write("Şehirinizi Girin: ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("İşleniyor...");
+            Console.WriteLine("Biti!");
+            
+            Console.WriteLine();
+
+            Console.WriteLine("**** Umut Lokanta- Rezerbasyon Kartı ****");
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("İletişim Bilgileri: " + customerPhone + "/" + customerEmail);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Adres Bilgileri: " + district + "/" + city);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("**** Umut Lokanta- Rezerbasyon Kartı ****");
+
+            Console.WriteLine();
+
+            Console.WriteLine("----  Yiyecek/İçecek Çeşitleri ----");
+            Console.WriteLine();
+            Console.WriteLine("1-Yiyecekler");
+            Console.WriteLine("2-İçecekler");
+            Console.WriteLine("3-Tatlılar");
+            Console.WriteLine("4-Salatalar");
+            Console.WriteLine();
+            Console.WriteLine("----  Yiyecek/İçecek Çeşitleri ----");
+
+            Console.WriteLine();
+
+            Console.Write("Bir kategori seçin: ");
+            int secim = int.Parse(Console.ReadLine());
+
+            if (secim == 1)
+            {
+                Console.WriteLine("----  Yiyecek Çeşitleri ----");
+                Console.WriteLine();
+                Console.WriteLine("1-Adana kebap");
+                Console.WriteLine("2-Lahmacun");
+                Console.WriteLine("3-Tavuk dürüm");
+                Console.WriteLine("4-Köfte");
+                Console.WriteLine("5-Kuru Fasülye/ Pilav");
+                Console.WriteLine();
+                Console.WriteLine("----  Yiyecek Çeşitleri ----");
+            }
+
+            else if (secim == 2)
+            {
+                Console.WriteLine("----  İçecek Çeşitleri ----");
+                Console.WriteLine();
+                Console.WriteLine("1-Kola");
+                Console.WriteLine("2-Fanta");
+                Console.WriteLine("3-Sprite");
+                Console.WriteLine("4-Ayran");
+                Console.WriteLine("5-Soda");
+                Console.WriteLine();
+                Console.WriteLine("----  İçecek Çeşitleri ----");
+            }
+
+            else if (secim == 3)
+            {
+                Console.WriteLine("----  Tatlı Çeşitleri ----");
+                Console.WriteLine();
+                Console.WriteLine("1-Künefe");
+                Console.WriteLine("2-Dondurma");
+                Console.WriteLine("3-Suffle");
+                Console.WriteLine("4-Baklava");
+                Console.WriteLine();
+                Console.WriteLine("----  Tatlı Çeşitleri ----");
+            }
+            else if (secim == 4)
+            {
+                Console.WriteLine("----  Salata Çeşitleri ----");
+                Console.WriteLine();
+                Console.WriteLine("1-Salata");
+                Console.WriteLine("2-Soğanlı Salata");
+                Console.WriteLine();
+                Console.WriteLine("----  Salata Çeşitleri ----");
+            }
+            else
+            {
+                Console.WriteLine("Hata! yanlık değer girdiniz, lütfen programı yeniden başlatın.");
+            }
+            int cost = 500;
+            Console.WriteLine("Menümüzü İncelediğiniz için Teşekkür ederiz! Bizden %30 indirim kazandınız!!");
+            Console.WriteLine("Hesabınız " + cost + "TL den " + cost*0.30 + "TL ye düştü!");
+            double cost2 = cost * 0.30;
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+//Yazdırma kıomutları
---
+++ 02_RestaurantProject/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_RestaurantProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopContiuning = true;
+            List<int> orders = new List<int>();
+            List<string> orderedFoodNames = new List<string>();
+            string[] foods = { "Adana kebap", "Lahmacun", "Pizza", "Hamburger", "Mantı" };
+            string[] drinks = { "Kola", "Fanta", "İce tea", "Sprite", "Soda" };
+            string[] desserts = { "Künefe", "Dondurma" };
+            int[] foodprices = { 320, 250, 200, 210, 100 };
+            int[] drinkprices = { 50, 50, 30, 50, 40 };
+            int[] dessertprices = { 50, 50 };
+
+            Console.WriteLine("---- UMUT LOKANTA PRO 2.0 ----");
+
+            while (loopContiuning)
+            {
+                Console.WriteLine("**** Kategoriler ****");
+                Console.WriteLine("1- Yemekler");
+                Console.WriteLine("2- İçecekler");
+                Console.WriteLine("3- Tatlılar");
+                Console.WriteLine("4- Çıkış");
+                Console.WriteLine("5-Hesabı Topla");
+                Console.WriteLine("**** Kategoriler ****");
+
+                Console.WriteLine();
+
+                Console.Write("Kategori Seçin: ");
+                string desicion = Console.ReadLine();
+
+                switch (desicion)
+                {
+                    case  "1":
+                        for (int i = 0; i < foods.Length; i++)
+                        {
+                            Console.WriteLine($"{i}-{foods[i]}");
+                        }
+                        Console.Write("Sipariş vermek istediğiniz yiyeceğin numarasını yazın;");
+                        int fooddec  = int.Parse(Console.ReadLine());
+                        orders.Add(foodprices[fooddec]);
+                        orderedFoodNames.Add(foods[fooddec]);
+                        Console.WriteLine($"{foods[fooddec]}-{foodprices[fooddec]}TL- Siparişiniz başarı ile eklendi!");
+                        break;
+                    case "2":
+                        for(int i = 0; i<drinks.Length;i++)
+                        {
+                            Console.WriteLine($"{i}-{drinks[i]}");
+                        }
+                        Console.Write("Sipariş vermek istediğiniz içeceğin numarasını yazın;");
+                        int drinkdec = int.Parse(Console.ReadLine());
+                        orders.Add(drinkprices[drinkdec]);
+                        orderedFoodNames.Add(drinks[drinkdec]);
+                        Console.WriteLine($"{drinks[drinkdec]}-{drinkprices[drinkdec]}TL- Siparişiniz başarı ile eklendi!");
+                        break;
+                    case "3":
+                        for(int i = 0; i<desserts.Length; i++)
+                        {
+                            Console.WriteLine($"{i}-{desserts[i]}");
+                        }
+                        Console.Write("Sipariş vermek istediğiniz tatlının numarasını yazın;");
+                        int dessertdec = int.Parse(Console.ReadLine());
+                        orders.Add(dessertprices[dessertdec]);
+                        orderedFoodNames.Add(desserts[dessertdec]);
+                        Console.WriteLine($"{desserts[dessertdec]}-{dessertprices[dessertdec]}Siparişiniz başarı ile eklendi!");
+                        break;
+                    case "4":
+                        Console.WriteLine("Yeniden bekleriz!");
+                        loopContiuning = false;
+                        break;
+                    case "5":
+                        int hesap = 0;
+                        foreach(string food in orderedFoodNames)
+                        {
+                            Console.WriteLine($"-{food}");
+
+                        }
+                        foreach (int totalcheck in orders)
+                        {
+                          hesap += totalcheck;
+                        }
+                        Console.WriteLine($"Hesabınız {hesap} TL!");
+                        break;
+                    default:
+                        Console.WriteLine("Hata, lütfen tekrar deneyin.");
+                        break;
+                }
+
+            }
+        } 
+    }
+}
